6974f469039e545786b7b4fad113251030f3fc36
sithumoemyint-code <sithumoemyint.1331@gmail.com>
2023-09-11 13:45:06 +0630

    some amendment

--- src/components/products/ProductContactService.jsx
+++ src/components/products/ProductContactService.jsx
@@ -8,7 +8,7 @@ const ProductContactService = () => {
         <div style={{ backgroundImage: `url(https://orenda.vn/wp-content/themes/flatsome-child/img/gia-cong-img-11.png)` }}  className='w-full h-full bg-center bg-cover duration-500' />
       </div>
       <div className=' '>
-        <div className='absolute top-0 left-0 right-0 bottom-0 py-10'>
+        <div className='absolute top-0 left-0 right-0 bottom-0 py-5'>
           <div className='global-container'>
             <span className='text-xl font-bold'>Contact to use the service</span>
             <div className='mt-3 p-3 rounded bg-cutBGColor w-24 flex items-baseline text-white font-bold'>
